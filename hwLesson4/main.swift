2c9b1ea95e9a42ab2d6f18eac0188c1a51f02b4b
Alexandr205 <sanchez.kalashnikov.2005@gmail.com>
2022-05-18 16:21:20 +0600

    gg

--- hwLesson4/main.swift
+++ hwLesson4/main.swift
@@ -7,5 +7,28 @@
 
 import Foundation
 
-print("Hello, World!")
+var names = ["Alex", "Aidar", "Nursultan", "Emil"]
 
+var checkName = true
+
+print("Enter ur name:")
+var name = readLine()
+
+
+for var i in 0..<names.count {
+    if names.contains(name!){
+        print("Well done")
+        checkName = false
+        i = names.count
+        break
+    }else {
+        print("We dont have this name in our database")
+        checkName = true
+        i -= 1
+    }
+}
+
+if checkName == false {
+    print("Name freely")
+    names.append(name!)
+}
